e46002aaae7fc6acd118023c08141af7ae4f9727
Vicky <vickynguyen143@gmail.com>
2023-02-04 00:33:51 -0600

    everything works, need to fix format

--- Homework1/Homework1_vtn180004.py
+++ Homework1/Homework1_vtn180004.py
@@ -9,7 +9,6 @@ import pickle
 
 
 class Person:
-
     # init method
     def __init__(self, firstName, lastName, mi, id, phoneNum):
         self.firstName = firstName
@@ -19,8 +18,11 @@ class Person:
         self.phoneNum = phoneNum
     
     # display method
+    # \n = new line, \t = new tab
     def display(self):
-        print('Employee id:' + id)
+        print('Employee id: ' + self.id
+        + '\n\t' + self.firstName + self.mi + self.lastName
+        + '\n\t' + self.phoneNum)
 
 # modify last name and first name to be capitalized
 def modName(name):
@@ -36,7 +38,6 @@ def modMi(mi):
 
 # modify ID 
 def modId(id):
-
     # regex, ID correct format: 2 letters followed by 4 digits
     if re.search('[a-zA-Z][a-zA-Z]\d\d\d\d', id) and len(id) == 6:
         return id
@@ -50,47 +51,54 @@ def modId(id):
 
 # modify phone number, if necessary
 def modPhoneNum(phoneNum):
-
     # regex, phone number correct format: 999-999-9999
     if re.search('\d\d\d-\d\d\d-\d\d\d\d', phoneNum) and len(phoneNum) == 12:
         return phoneNum
     else:
         # phone number not in correct format, output error msg
         # allow user to input a valid phone number
-        id = input('Phone: ' + phoneNum + 'is invalid'
+        phoneNum = input('Phone: ' + phoneNum + ' is invalid'
         + '\nEnter phone number in the form 123-456-7890'
         + '\nEnter phone number: ')
     return modPhoneNum(phoneNum)
 
-# check for duplicate id and print and error msg if an id is repeated in the file
-def checkDup(dict, key):
-    if 
+# check for duplicate ID, print error msg if ID is repeated in the file
+def checkDupID(dict, idKey):
+    if idKey in dict.keys():
+        print('Error: Duplicate ID ' + idKey
+        + ', will not add in dictionary') 
+        return True    
     return False
 
 # create function to process input file, get rid of the first line which is the heading line
 def processFile(txt_in):
+    # employee dictionary set up
+    empDict = {} 
 
     # split on comma to get fields
     for line in txt_in:
         fields = line.split(',')
 
-    # modify first, last, mi, id, phone number if necessary
-    firstName = modName(fields[1])
-    mi = modMi(fields[2])
-    lastName = modName(fields[0])
-    id = modId(fields[3])
-    #phoneNum = modPhoneNum(fields[4])
-    
+        # modify first, last, mi, id, phone number if necessary
+        firstName = modName(fields[1])
+        mi = modMi(fields[2])
+        lastName = modName(fields[0])
+        id = modId(fields[3])
+        phoneNum = modPhoneNum(fields[4])
+        
+        # if there is not a duplicate ID, create dictionary
+        # dict format = key:value, id : person (Object) 
+        if not checkDupID(empDict, id):
+            # person object
+            person = Person(firstName, lastName, mi, id, phoneNum)
+            # update dictionary
+            empDict.update({id : person})
+            
+    # return dict of persons to main function
+    return empDict
 
-    # once data for a person is correct, create a Person object and save the object to a dict of persons
-    # where id is the key. 
 
-    # return the dict of persons to the main function
-    return
-
-
-
-# ----------- main -----------
+# ------------ main ------------
 if __name__ == '__main__':
     
     # check if there's a sysarg
@@ -108,16 +116,16 @@ if __name__ == '__main__':
 
     # call process function and ignore the header line
     # place all people from data input into 'employees' dict 
-    employees = processFile(txt_in[1:])
+    empDict = processFile(txt_in[1:])
 
     # pickle file, write back
-    pickle.dump(employees, open('employees.pickle', 'wb'))
+    pickle.dump(empDict, open('empDict.pickle', 'wb'))
 
     # unpickle file, read back
-    employees_in = pickle.load(open('employees.pickle', 'rb'))
+    employees_in = pickle.load(open('empDict.pickle', 'rb'))
 
     # print employee list to check if successful unpickle
     print('\n\nEmployee list:')
 
-    # for id in employees_in:
-    #    employees_in[id].display()
+    for person in employees_in:
+        employees_in[person].display()
