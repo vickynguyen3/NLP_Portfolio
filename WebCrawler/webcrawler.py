848cfeab8df7ca1f0a33b71f9f7606a22eee24ea
Vicky <vickynguyen143@gmail.com>
2023-03-08 22:42:24 -0600

    setup

---
+++ WebCrawler/webcrawler.py
@@ -0,0 +1,19 @@
+# Vicky Nguyen, vtn180004
+# Meinhard
+#
+
+# web crawler function
+# starts with a URL representing a topic
+# outputs list of at least 15 relevant URLs
+
+# function to loop thru URLs and scrape all text off each page
+# store each page's text in its own file
+
+# function to clean up the text from each file
+# delete newlines and tabs first
+# extract sentences w/ NLTK's sentence tokenizer
+# write sentences for each file to a new file
+# e.g. 15 files in, 15 files out
+
+
+# ---- main ----
