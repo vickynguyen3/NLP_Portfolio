b0b900f305058474aebf492b596777a633ea0907
Vicky <vickynguyen143@gmail.com>
2023-04-07 02:36:16 -0500

    import

--- Chatbot/chatbot.py
+++ Chatbot/chatbot.py
@@ -2,3 +2,12 @@
 # CS 4395.001
 # Chatbot Project
 
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import numpy
+import random
+import json
+import pickle
+
